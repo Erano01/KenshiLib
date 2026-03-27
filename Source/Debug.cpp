b05c6f03c1805815aff6a0635149fc6640b83253
Bryden Frizzell <bryden.frizzell@gmail.com>
2026-03-28 09:42:48 +1300

    Added module names to debug prints

--- Source/Debug.cpp
+++ Source/Debug.cpp
@@ -7,33 +7,26 @@
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
 #include <Windows.h>
+#include <intrin.h>
+#include <Shlwapi.h>
 
 std::stringstream debugLog;
 std::ofstream debugFile("RE_Kenshi_log.txt");
 clock_t startTime = -1;
 
-void DebugLog(const std::string& message)
+static std::string GetModuleName(void* address)
 {
-	if (startTime == -1)
-		startTime = clock();
-
-	clock_t logTime = clock();
-	clock_t timeInMSecs = (logTime-startTime) / (CLOCKS_PER_SEC / 1000);
-
-	char timeStr[10];
-	sprintf_s(timeStr, "%i.%03i", timeInMSecs/1000, timeInMSecs%1000);
-
-	// TODO disable?
-	debugFile << timeStr << ": " << message << std::endl;
-	debugLog << timeStr << ": " << message << std::endl;
+	// extract DLL name of caller without extension
+	HMODULE module;
+	GetModuleHandleExA(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS, (LPCSTR)address, &module);
+	char modulePath[_MAX_PATH];
+	GetModuleFileNameA(module, modulePath, sizeof(modulePath));
+	char* moduleFileName = PathFindFileNameA(modulePath);
+	PathRemoveExtensionA(moduleFileName);
+	return moduleFileName;
 }
 
-void DebugLog(const char* message)
-{
-	DebugLog(std::string(message));
-}
-
-void ErrorLog(const std::string& message)
+static void DebugLog(const std::string& moduleFileName, const std::string& message)
 {
 	if (startTime == -1)
 		startTime = clock();
@@ -44,13 +37,44 @@ void ErrorLog(const std::string& message)
 	char timeStr[10];
 	sprintf_s(timeStr, "%i.%03i", timeInMSecs / 1000, timeInMSecs % 1000);
 
-	debugFile << "Error " << timeStr << ": " << message << std::endl << std::flush;
-	debugLog << "Error " << timeStr << ": " << message << std::endl;
+	// TODO disable?
+	debugFile << timeStr << " " << moduleFileName << ": " << message << std::endl;
+	debugLog << timeStr << " " << moduleFileName << ": " << message << std::endl;
+}
+
+void DebugLog(const std::string& message)
+{
+	DebugLog(GetModuleName(_ReturnAddress()), message);
+}
+
+void DebugLog(const char* message)
+{
+	DebugLog(GetModuleName(_ReturnAddress()), std::string(message));
+}
+
+static void ErrorLog(const std::string& moduleFileName, const std::string& message)
+{
+	if (startTime == -1)
+		startTime = clock();
+
+	clock_t logTime = clock();
+	clock_t timeInMSecs = (logTime - startTime) / (CLOCKS_PER_SEC / 1000);
+
+	char timeStr[10];
+	sprintf_s(timeStr, "%i.%03i", timeInMSecs / 1000, timeInMSecs % 1000);
+
+	debugFile << "Error " << timeStr << " " << moduleFileName << ": " << message << std::endl << std::flush;
+	debugLog << "Error " << timeStr << " " << moduleFileName << ": " << message << std::endl;
+}
+
+void ErrorLog(const std::string& message)
+{
+	ErrorLog(GetModuleName(_ReturnAddress()), message);
 }
 
 void ErrorLog(const char* message)
 {
-	ErrorLog(std::string(message));
+	ErrorLog(GetModuleName(_ReturnAddress()), std::string(message));
 }
 
 std::string GetDebugLog()
