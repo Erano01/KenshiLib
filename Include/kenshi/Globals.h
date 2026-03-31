e044c28bbe3cabd0a2ff78295a81abec1ad90f99
Bryden Frizzell <bryden.frizzell@gmail.com>
2026-04-01 12:22:53 +1300

    Added missing globals

--- Include/kenshi/Globals.h
+++ Include/kenshi/Globals.h
@@ -5,9 +5,20 @@ class GameWorld;
 class GlobalConstants;
 class InputHandler;
 class OptionsHolder;
+class GameData;
+class GameDataContainer;
+
+// TODO
+namespace FoliageSystem
+{
+	class EntData;
+}
 
 void  showErrorMessage();// RVA = 0x5CC110
 float modMedicalSkill(float skill, Item* equipment, float frameTIME);// RVA = 0x4FC9E0
+void getFoliageRotation(FoliageSystem::EntData* data, float x, float z, Ogre::Quaternion& rotation);// RVA = 0x6CB8A0
+GameData* getBuildingCollection(GameDataContainer& c);// RVA = 0x385380
+void increaseStat(float& stat, float amount, float UPPER_LIMIT);// RVA = 0x8C4640
 
 __declspec(dllimport) GameWorld* ou;
 __declspec(dllimport) GlobalConstants* con;
